9341ac4ec59df3263bfa8f43f7f39f128a56855b
romi <108284206+romieeeee@users.noreply.github.com>
2025-08-05 17:26:33 +0900

    [Bronze II] Title: 상수, Time: 120 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		a = a%10*100 + (a/10)%10*10 + (a/100);
+		b = b%10*100 + (b/10)%10*10 + (b/100);
+		
+		if(a>b) {
+			System.out.println(a);
+		}else {
+			System.out.println(b);
+		}
+	}
+
+}
