e74d9cc5d52e7c46ecedb480049b4471c5c9bbf0
romi <108284206+romieeeee@users.noreply.github.com>
2025-08-05 09:29:22 +0900

    Update 체스판 다시 칠하기.py

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,28 +1,50 @@
-# 백준 1018
 n, m = map(int, input().split())
-board = [list(x for x in input()) for _ in range (n)]
 
-# 색칠해야하는 경우의 수 
-# 행 별로 검사하면 된다
-paint = [['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], 
-         ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']] 
+board = [list(input()) for _ in range (n)]
 
 min_cnt = n*m
-for i in range(n - 7):
-    for j in range(m - 7):
-        cnt1 = cnt2 = 0
-        for x in range(8):
-            for y in range(8):
-                if (x + y) % 2 == 0:
-                    if board[i + x][j + y] != 'W':
-                        cnt1 += 1
-                    if board[i + x][j + y] != 'B':
-                        cnt2 += 1
-                else:
-                    if board[i + x][j + y] != 'B':
-                        cnt1 += 1
-                    if board[i + x][j + y] != 'W':
-                        cnt2 += 1
+for i in range (n-7): # m-8+1
+    for j in range (m-7):
+
+        cnt1 = 0  # 'W'로 시작할 때
+        cnt2 = 0  # 'B'로 시작할 때
+
+        for x in range (8):
+            for y in range (8):
+                cur = board[i+x][j+y]
+
+                # 'W' 시작 체스판일 때 
+                if x % 2 == 0:  
+                    if y % 2 == 0:  # 짝수 열
+                        if cur != 'W':
+                            cnt1 += 1
+                    else:  # 홀수 열
+                        if cur != 'B':
+                            cnt1 += 1
+                else:  # 홀수 행
+                    if y % 2 == 0:  # 짝수 열
+                        if cur != 'B':
+                            cnt1 += 1
+                    else:  # 홀수 열
+                        if cur != 'W':
+                            cnt1 += 1
+
+                # 'B' 시작 체스판일 때 
+                if x % 2 == 0:  # 짝수 행
+                    if y % 2 == 0:  # 짝수 열
+                        if cur != 'B':
+                            cnt2 += 1
+                    else:  # 홀수 열
+                        if cur != 'W':
+                            cnt2 += 1
+                else:  # 홀수 행
+                    if y % 2 == 0:  # 짝수 열
+                        if cur != 'W':
+                            cnt2 += 1
+                    else:  # 홀수 열
+                        if cur != 'B':
+                            cnt2 += 1
+
         min_cnt = min(min_cnt, cnt1, cnt2)
 
 print(min_cnt)
