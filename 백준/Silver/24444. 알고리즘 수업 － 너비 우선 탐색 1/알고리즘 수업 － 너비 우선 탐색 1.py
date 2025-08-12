e938dc03c6e2049dfb77c0c731ea025ab702fc44
romi <108284206+romieeeee@users.noreply.github.com>
2025-08-12 14:13:05 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 516 ms, Memory: 62168 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+
+def bfs():
+    global cnt, order
+    while q:
+        cur = q.popleft()
+
+        for nxt in sorted(graph[cur]):  
+            if not visited[nxt]:
+                visited[nxt] = True
+                q.append(nxt)
+                cnt += 1
+                order[nxt] = cnt
+
+
+
+n, m, r = map(int, sys.stdin.readline().split()) # 정점, 간선, 시작 정점
+
+graph = [[] for _ in range (n+1)]
+visited = [False] * (n+1)
+
+for _ in range (m):
+    u, v = map(int, sys.stdin.readline().split())
+
+    # 무방향 그래프
+    graph[u].append(v)
+    graph[v].append(u)
+
+q = deque()
+
+q.append(r)
+visited[r] = True
+
+cnt = 1
+
+order = [0] * (n+1)
+order[r] = cnt
+
+bfs() 
+
+for o in order[1:]:
+    print(o)
