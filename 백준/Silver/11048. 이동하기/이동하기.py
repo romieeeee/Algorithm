63d02e447832736221a848aeb71817a169d97f56
romi <108284206+romieeeee@users.noreply.github.com>
2025-09-05 14:58:32 +0900

    [Silver II] Title: 이동하기, Time: 912 ms, Memory: 80568 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -1,15 +1,14 @@
 n, m = map(int, input().split())
-candy = [list(map(int, input().split())) for _ in range (n)]
+maze = [list(map(int, input().split())) for _ in range (n)]
 
-dp = [[0] * m for _ in range (n)]
-dp[0][0] = candy[0][0]
+dp = [[0] * m for _ in range (n)] # i, j까지 갔을 때의 최대 사탕 개수
+dp[0][0] = maze[0][0]
 
 for i in range (n):
   for j in range (m):
-    top = dp[i-1][j] if i > 0 else 0
-    left = dp[i][j-1] if j > 0 else 0
-    diag = dp[i-1][j-1] if i > 0 and j > 0 else 0
-
-    dp[i][j] = max(top, left, diag) + candy[i][j]
+    top = dp[i-1][j] if i>0 else 0
+    bottom = dp[i][j-1] if j>0 else 0
+    diag = dp[i-1][j-1] if i >0 and j>0 else 0
+    dp[i][j] = max(top, bottom, diag) + maze[i][j]
 
 print(dp[n-1][m-1])
