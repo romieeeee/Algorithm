744c1a51f6076c5dd59eba77108f4a49f94a5261
romi <108284206+romieeeee@users.noreply.github.com>
2025-09-09 11:48:09 +0900

    [Gold V] Title: 퇴사 2, Time: 824 ms, Memory: 212360 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -0,0 +1,22 @@
+import sys
+imput = sys.stdin.readline
+
+n = int(input())
+
+time = []
+price = []
+
+for _ in range (n):
+  t, p = map(int, input().split())
+  time.append(t)
+  price.append(p)
+
+dp = [0] * (n+1) # i일에 얻을 수 있는 최대 수익
+
+for i in range (n-1, -1, -1):
+    if i + time[i] <= n:
+       dp[i] = max(dp[i+1], price[i]+dp[i+time[i]])
+    else:
+       dp[i] = dp[i+1]
+  
+print(dp[0])
