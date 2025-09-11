d5ff6d9a9586def30b14eb08f9af8dfb551db9c1
romi <108284206+romieeeee@users.noreply.github.com>
2025-09-11 16:40:20 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 2932 ms, Memory: 122480 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+arr_sum = [0] * (n + 1)
+
+for i in range(n):
+    arr_sum[i + 1] += arr_sum[i] + arr[i]
+
+for _ in range(m):
+    i, j = map(int, input().split())
+
+    if i == j:
+        print(arr[i - 1])
+    else:
+        print(arr_sum[j] - arr_sum[i - 1])
