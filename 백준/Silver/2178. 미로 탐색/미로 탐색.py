3270b8837a1b0a11c60a90d9a4e874272d6c0a2e
romi <108284206+romieeeee@users.noreply.github.com>
2025-09-17 16:47:55 +0900

    [Silver I] Title: 미로 탐색, Time: 124 ms, Memory: 114468 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,31 +1,60 @@
 from collections import deque
 
-def bfs(x, y):
-    q = deque()
-    q.append([x, y])
+d = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+def dfs(x, y, dist):
+    global min_dist
+    
+    if dist > min_dist:
+        return
+    
+    if (x, y) == (n-1, m-1):
+        if dist < min_dist: min_dist = dist
+        return
+    
+    for dx, dy in d:
+        nx, ny = x+dx, y+dy
+        
+        if 0<=nx<n and 0<=ny<m and not visited[nx][ny]:
+            if graph[nx][ny] == 1:
+                visited[nx][ny] = True
+                dfs(nx, ny, dist+1) 
+                visited[nx][ny] = False
+                
 
+def bfs():
+    q.append([0, 0])
+    visited[0][0] = 1
+    
     while q:
         x, y = q.popleft()
-
-        for i in range (4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            # 길이 있고, 아직 방문하지 않았다면
-            if 0 <= nx < n and 0 <= ny < m:
-                if maze[nx][ny] == 1:
-                    maze[nx][ny] = maze[x][y] + 1
-
-                    q.append([nx, ny])
-
-    print(maze[n-1][m-1])
+        
+        if (x, y) == (n-1, m-1):
+            print(visited[x][y])
+            return
+        
+        for dx, dy in d:
+            nx, ny = x+dx, y+dy
+        
+            if 0<=nx<n and 0<=ny<m:
+                if graph[nx][ny] == 1 and visited[nx][ny] == 0:
+                        visited[nx][ny] = visited[x][y] + 1
+                        q.append([nx, ny])
 
 n, m = map(int, input().split())
-maze = [list(map(int, input().strip())) for _ in range(n)]
+graph = [[int(x) for x in input()] for _ in range (n)]
 
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+# -- dfs --
+# min_dist = 1e9
+# visited = [[False] * m for _ in range (n)]
 
-min_steps = float('inf')
+# visited[0][0] = True
+# dfs(0, 0, 1)
 
-bfs(0, 0)
+# print(min_dist)
+
+# -- bfs --
+visited = [[0] * m for _ in range (n)]
+
+q = deque()
+
+bfs()
