60bc9520fb46ca903addbc11851aded407446ce6
romi <108284206+romieeeee@users.noreply.github.com>
2025-09-25 22:27:30 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 176 ms, Memory: 112108 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,34 +1,41 @@
 import sys
+
 input = sys.stdin.readline
 
 def dfs(depth, value, plus, minus, mul, div):
-    global max_res, min_res
-    if depth == n:  
-        max_res = max(max_res, value)
-        min_res = min(min_res, value)
-        return
+    global max_val, min_val
 
+    if depth == n:
+        max_val = max(max_val, value)
+        min_val = min(min_val, value)
+        return
+    
     if plus > 0:
-        dfs(depth+1, value + nums[depth], plus-1, minus, mul, div)
+        dfs(depth+1, value+nums[depth], plus-1, minus, mul, div)
+    
     if minus > 0:
-        dfs(depth+1, value - nums[depth], plus, minus-1, mul, div)
+        dfs(depth+1, value-nums[depth], plus, minus-1, mul, div)
+    
     if mul > 0:
-        dfs(depth+1, value * nums[depth], plus, minus, mul-1, div)
+        dfs(depth+1, value*nums[depth], plus, minus, mul-1, div)
+    
     if div > 0:
         if value < 0:
-            dfs(depth+1, -(-value // nums[depth]), plus, minus, mul, div-1)
+            tmp = -(-value//nums[depth])
         else:
-            dfs(depth+1, value // nums[depth], plus, minus, mul, div-1)
+            tmp = value//nums[depth]
+            
+        dfs(depth+1, tmp, plus, minus, mul, div-1)
 
-n = int(input())
+
+n = int(input()) # 수의 개수
 nums = list(map(int, input().split()))
-ops = list(map(int, input().split()))  # [+, -, *, /] 
+ops = list(map(int, input().split())) # +, -, *, / 
 
-max_res = -int(1e9)  
-min_res = int(1e9)
+max_val = -1e9
+min_val = 1e9
 
 dfs(1, nums[0], ops[0], ops[1], ops[2], ops[3])
 
-# 결과 출력
-print(max_res)
-print(min_res)
+print(int(max_val))
+print(int(min_val))
