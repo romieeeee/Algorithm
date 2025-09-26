9778b3e151ce9ea9d578f81ce5669c33d3d3353a
romi <108284206+romieeeee@users.noreply.github.com>
2025-09-26 23:31:51 +0900

    [Gold V] Title: 보물섬, Time: 812 ms, Memory: 117716 KB -BaekjoonHub

---
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -0,0 +1,45 @@
+import sys
+from collections import deque
+
+directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+def isLand(x, y):
+    if graph[x][y] == 'L':
+        return True
+        
+
+def bfs(x, y):
+    q = deque()
+    q.append((x, y))
+    visited = [[0] * m for _ in range (n)]
+    visited[x][y] = 1
+
+    value = 0
+    while q:
+        cx, cy = q.popleft()
+
+        for dx, dy in directions:
+            nx, ny = cx+dx, cy+dy
+            if 0<=nx<n and 0<=ny<m:
+                if not visited[nx][ny] and graph[nx][ny] == 'L':
+                    q.append((nx, ny))
+                    visited[nx][ny] = visited[cx][cy] + 1
+
+                    if visited[nx][ny] > value: value = visited[nx][ny]
+
+    return value
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = [[x for x in input().strip()] for _ in range (n)] # 'L' or 'W'
+
+max_res = 0
+for i in range (n):
+    for j in range (m):
+        if graph[i][j] == 'L':
+            res = bfs(i, j)
+
+            if res > max_res: max_res = res
+
+print(max_res-1)
