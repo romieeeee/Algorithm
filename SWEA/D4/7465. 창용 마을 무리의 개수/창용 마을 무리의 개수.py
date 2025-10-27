f2d308d9afc1fd7420856271678717574880af8e
romi <108284206+romieeeee@users.noreply.github.com>
2025-10-27 14:46:15 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 116 ms, Memory: 61,440 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.py
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.py
@@ -1,26 +1,28 @@
-def dfs(x):
-  visited[x] = True
-  for nxt in graph[x]:
-      if not visited[nxt]:
-          dfs(nxt)
-
 T = int(input())
-for test_case in range(1, T + 1):
+
+def dfs(v):
+  visited[v] = True
+  
+  for nxt in graph[v]:
+    if not visited[nxt]:
+      dfs(nxt)
+
+
+for test_case in range (1, T+1):
   n, m = map(int, input().split())
-  graph = [[] for _ in range(n+1)] 
-
-  visited = [False] * (n+1)
-
-  cnt = 0 # 무리의 개수
-
-  for _ in range(m):
+  graph = [[] for _ in range (n+1)]
+  visited = [False] * (n+1) 
+  
+  for _ in range (m):
     u, v = map(int, input().split())
-    graph[u].append(v)  
+    graph[u].append(v)
     graph[v].append(u)
-
+  
+  cnt = 0  
   for i in range (1, n+1):
     if not visited[i]:
-      cnt += 1
       dfs(i)
-    
+      cnt += 1
+  
   print(f'#{test_case} {cnt}')
+  
