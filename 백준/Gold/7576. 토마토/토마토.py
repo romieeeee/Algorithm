740bad1c1ef227b9fa220a58eacb9e42495628ce
romi <108284206+romieeeee@users.noreply.github.com>
2025-11-23 18:27:22 +0900

    [Gold V] Title: 토마토, Time: 328 ms, Memory: 156796 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,28 +1,32 @@
 from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
 def bfs():
     while q:
         x, y = q.popleft()
 
-        for dx, dy in dir:
-            nx, ny = x+dx, y+dy
+        for i in range(4):
+            nx, ny = x+dx[i], y+dy[i]
 
-            if 0<=nx<n and 0<=ny<m and farm[nx][ny] == 0:
-                    farm[nx][ny] = farm[x][y] + 1
-                    q.append((nx, ny))  
-
-
-dir = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+            if 0<=nx<n and 0<=ny<m and graph[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y] + 1
+                q.append((nx, ny))
+    
 
 m, n = map(int, input().split())
-farm = [list(map(int, input().split())) for _ in range (n)]
+
+# 1: 익토 0: 안익토 -1: x
+graph = [list(map(int, input().split())) for _ in range (n)]
 
 q = deque()
-
 for i in range (n):
     for j in range (m):
-        if farm[i][j] == 1:
-            # 익은 토마토들을 queue에 넣는다
+        if graph[i][j] == 1:
             q.append((i, j))
 
 bfs()
@@ -32,11 +36,11 @@ for i in range(n):
     for j in range(m):
         
         # bfs 이후에도 아직 안 익은 토마토가 존재한다면
-        if farm[i][j] == 0: 
+        if graph[i][j] == 0: 
             print(-1)
             exit(0)
         
         # 토마토가 익을 때까지의 날짜 (max를 구하면 된다)
-        days = max(days, farm[i][j])
+        days = max(days, graph[i][j])
 
 print(days - 1) 
