fa9a871584581eacf3c8a7d94ba72e4d6b0e4139
romi <108284206+romieeeee@users.noreply.github.com>
2025-11-23 23:50:57 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 137 ms, Memory: 63,616 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
@@ -1,37 +1,39 @@
 from collections import deque
 
-d = [[1, 0], [0 , 1], [-1, 0], [0, -1]]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
 def bfs():
-  
-  while q:
-    x, y = q.popleft()
+    global min_time
 
-    for dx, dy in d:
-      nx, ny = x+dx, y+dy
+    q = deque()
+    q.append((0, 0)) # x, y, t
+    dist[0][0] = graph[0][0]
 
-      if 0<=nx<n and 0<=ny<n: # 범위 내에 있고
-        if dist[x][y] + graph[nx][ny] < dist[nx][ny]:
-          dist[nx][ny] = dist[x][y] + graph[nx][ny]
-          
-          q.append([nx, ny])
+    while q:
+        x, y = q.popleft()
+
+        for i in range (4):
+            nx, ny = x+dx[i], y+dy[i]
+
+            if nx<0 or nx>=n or ny<0 or ny>=n:
+                continue
+
+            if dist[x][y]+graph[nx][ny] < dist[nx][ny]:
+                dist[nx][ny] = dist[x][y]+graph[nx][ny]
+                q.append((nx, ny))
+
 
 T = int(input())
-for test_case in range(1, T + 1):
-  n = int(input())
-  graph = []
+for test_case in range (1, T+1):
+    n = int(input())
+    graph = []
+    for _ in range (n):
+        row = [int(s) for s in input()]
+        graph.append(row)
 
-  for _ in range (n):
-    row = [int(x) for x in input()]
-    graph.append(row)
+    dist = [[1e9] * n for _ in range (n)]
 
-  dist = [[1e9] * n for _ in range (n)]
-  dist[0][0] = graph[0][0]
+    bfs()
 
-  q = deque()
-  q.append([0, 0])
-
-  bfs()
-  print(f'#{test_case} {dist[n-1][n-1]}')
-
-    
+    print(f'#{test_case} {dist[n-1][n-1]}')
