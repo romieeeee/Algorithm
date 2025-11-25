962e3b08f3ea5a144568fbd577735421a8800237
romi <108284206+romieeeee@users.noreply.github.com>
2025-11-25 23:43:56 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 3,884 ms, Memory: 99,200 KB -BaekjoonHub

--- SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.py
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.py
@@ -1,35 +1,45 @@
-dx = [1,1,-1,-1]
-dy = [-1,1,1,-1]
+# 한 방향으로 3번 회전하는 경우의 수만 고려해도 결국 모든 경로를 탐색하게 된다
+d = [(1, 1), (-1, 1), (-1, -1), (1, -1)]
 
-def dfs(x, y, d, l, now, visited):
-  global res
-  
-  if l > 3: return
-  if l == 3 and (x == sx and  y == sy):
-    res = max(res, now)
-  else:   
+def dfs(x, y, di, turn, dessert):
+    global sx, sy, max_value
+
+    if turn > 3:
+        return
+    
+    if turn == 3 and x == sx and y == sy:
+        max_value = max(max_value, len(dessert))
+        return
+    
+    # 현재 방향을 유지하거나... 회전하거나...
     for i in range (2):
-      di = (d+i)%4
-      nx, ny = x+dx[di], y+dy[di]
-      
-      if nx < 0 or nx >= n or ny < 0 or ny >= n:
-        continue
-      
-      if graph[nx][ny] not in visited:
-        visited.append(graph[nx][ny])
-        dfs(nx, ny, di, l+i, now+1, visited)
-        visited.pop()
-  
+        ndir = (di+i)%4
+        nx, ny = x+d[ndir][0], y+d[ndir][1]
+
+        # 범위 내에 있고 아직 먹어본적 없는 디저트라면
+        if 0 <= nx < n and 0 <= ny < n:
+            if graph[nx][ny] in dessert:
+                continue
+
+            dessert.append(graph[nx][ny])
+
+            if i == 0: # 회전하지 않앗숭
+                dfs(nx, ny, ndir, turn, dessert)
+            else:
+                dfs(nx, ny, ndir, turn+1, dessert)
+
+            dessert.remove(graph[nx][ny])
+
 
 T = int(input())
 for test_case in range (1, T+1):
-  n = int(input())
-  graph = [list(map(int, input().split())) for _ in range (n)]
-  
-  res = -1
-  for i in range (n):
-    for j in range (n):
-      sx, sy = i, j
-      dfs(i, j, 0, 0, 0, []) # x, y, 현재 방향, 방향 전환 횟수, 방문한 디저트 가게
+    n = int(input())
+    graph = [list(map(int, input().split())) for _ in range (n)]
 
-  print(f'#{test_case} {res}')
+    max_value = -1
+    for i in range (n):
+        for j in range (n):
+            sx, sy = i, j
+            dfs(i, j, 0, 0, []) # x, y, 현재 방향 인덱스, 회전 횟수, 방문한 디저트
+
+    print(f'#{test_case} {max_value}')
