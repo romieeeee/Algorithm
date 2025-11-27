e3c244a6096f781c04a08b4e1597502d94941ebf
romi <108284206+romieeeee@users.noreply.github.com>
2025-11-27 10:41:40 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 454 ms, Memory: 65,408 KB -BaekjoonHub

--- SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.py
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.py
@@ -1,50 +1,73 @@
-def get_max_profit(idx, total, profit):
-    global max_profit
-    if total > c:  
-        return
-    
-    if idx == m: 
-        max_profit = max(max_profit, profit)
-        return
-
-    # 현재 꿀 선택
-    get_max_profit(idx+1, total + tmp_arr[idx], profit + tmp_arr[idx]**2)
-   
-    # 현재 꿀 선택하지 않음
-    get_max_profit(idx+1, total, profit)
-
-
+def dfs(start, depth, arr, amount, profit):
+  global end, max_amount, max_profit
+  
+  if amount > c:
+    return
+  
+  if depth == end:
+    max_amount = max(max_amount, amount)
+    max_profit = max(max_profit, profit)
+    return
+  
+  for i in range (start, m):
+    if not visited[i]:
+      visited[i] = True
+      profit += arr[i] ** 2 
+      
+      dfs(i, depth+1, arr, amount+arr[i], profit)
+      
+      profit -= arr[i] ** 2 
+      visited[i] = False
+      
 T = int(input())
-for test_case in range(1, T+1):
-    # 벌통의 크기, 선택할 수 있는 벌통의 개수, 채취할 수 있는 최대 양
-    n, m, c = map(int, input().split())
+for tc in range (1, T+1):
+  n, m, c = map(int, input().split())
+  graph = [list(map(int, input().split())) for _ in range (n)]
+  
+  max_h1 = 0
+  total = 0
+  res = 0
+  
+  for i in range (n):
+    for j in range (n-m+1):
+      h1 = graph[i][j:j+m]
+      
+      max_amount = 0
+      max_profit = 0      
+      for k in range (1, m+1):
+        end = k
+        visited = [False] * m
+        
+        dfs(0, 0, h1, 0, 0) # start, depth, arr, amount, profit
+        
+        # if max_amount == c:
+        #   break
+        
+      max_h1 = max_profit # h1의 최대 수익
 
-    honey = [list(map(int, input().split())) for _ in range (n)]
-    profit = [[0] * n for _ in range (n)]
-
-
-    tmp_arr = []
-    for i in range (n):
-        for j in range (n-m+1):
-            # m개의 벌통 선택
-            tmp_arr = honey[i][j:j+m]
-
-            max_profit = 0
-            get_max_profit(0, 0, 0)
-
-            profit[i][j] = max_profit
-
-
-    res = 0
-    for i in range(n):
-        for j in range(n-m+1):
-            for x in range(i, n):
-                # 같은 행이면 겹치지 않게
-                if i == x:
-                    start = j+m
-                else:
-                    start = 0
-                for y in range(start, n - m + 1):
-                    res = max(res, profit[i][j] + profit[x][y])
-
-    print(f"#{test_case}", res)
+      max_h2 = 0
+      for x in range (n):
+        for y in range (n-m+1):
+          # 같은 행에 있을 때 안겹치게
+          if i == x and y < j+m:
+            continue
+          
+          h2 = graph[x][y:y+m]
+          
+          max_amount = 0
+          max_profit = 0
+          for k in range (1, m+1):
+            end = k
+            visited = [False] * m
+            
+            dfs(0, 0, h2, 0, 0) # start, depth, arr, amount, profit
+            
+            # if max_amount == c:
+            #   break
+          max_h2 = max(max_h2, max_profit)
+      
+      total = max(total, max_h1+max_h2)
+      
+    res = max(res, total)
+  
+  print(f'#{tc} {res}')
