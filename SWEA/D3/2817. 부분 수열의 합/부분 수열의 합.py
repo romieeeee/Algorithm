45f87d9d165645c4c6a9ef6914ca2ce39c258ec8
romi <108284206+romieeeee@users.noreply.github.com>
2025-11-28 15:59:02 +0900

    [D3] Title: 부분 수열의 합, Time: 743 ms, Memory: 77,056 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -1,32 +1,23 @@
-def dfs(start, depth, now):
+def dfs(idx, now):
   global cnt
-  if depth > n:
+  
+  if idx == n:
+    if now == k:
+      cnt += 1
     return
   
-  if now == k:
-    cnt += 1
-    return
-    
-  for i in range (start, n):
-    if not visited[i]:
-      visited[i] = True
-      comb[depth] = a[i]
-      dfs(i, depth+1, now+a[i])
-      visited[i] = False
+
+  dfs(idx+1, now+a[idx])
+  dfs(idx+1, now)
     
 
 T = int(input())
 for tc in range (1, T+1):
   n, k = map(int, input().split())
   a = list(map(int, input().split()))
-  
-  visited = [False] * n
-  
+    
   cnt = 0
-  visited[0] = False
-  
-  comb = [0] * n
-  dfs(0, 0, 0)
+  dfs(0, 0)
   
   print(f'#{tc} {cnt}')
   
