ebc619e4b1e348c281ce2058abcc01093c688cf5
romi <108284206+romieeeee@users.noreply.github.com>
2026-02-22 19:44:58 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 106 ms, Memory: 61,440 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.py
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.py
@@ -1,28 +1,30 @@
-def dfs(idx):
-  for nxt in graph[idx]:
-    if not visited[nxt]:
-      visited[nxt] = True
-      dfs(nxt)
+def dfs(v):
+    for i in graph[v]:
+        if not visited[i]:
+            visited[i] = True
+            dfs(i)
+
 
 T = int(input())
-for tc in range (1, T+1):
-  n, m = map(int, input().split()) # 개수, 제한 칼로리
-  
-  graph = [[] * n for _ in range (n+1)]
-  visited = [False] * (n+1)
-  
-  for _ in range (m):
-    u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
-  
-  total = 0
-  for i in range (1, n+1):
-    if not visited[i]:
-      visited[i] = True
-      
-      dfs(i)
-      total += 1
-  
-  print(f'#{tc} {total}')
-  
+for test_case in range (1, T+1):
+    n, m = map(int, input().split())
+
+    graph = [[] * (n+1) for _ in range (n+1)]
+    visited = [False] * (n+1)
+
+    for _ in range (m):
+        u, v = map(int, input().split())
+
+        graph[u].append(v)
+        graph[v].append(u)
+
+    res = 0
+    for i in range (1, n+1):
+        if not visited[i]:
+            res += 1
+            
+            visited[i] = True
+            dfs(i)
+
+            
+    print(f'#{test_case} {res}')
